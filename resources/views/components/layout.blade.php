e420ed827687a9be4139368b553ec8228d8f859e
Jonathan Hailu <jonathanhailu3@gmail.com>
2025-05-22 12:10:43 +0300

    learned layout & lots

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -24,3 +24,29 @@
     </main>
 </body>
 </html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ninja Network</title>
+</head>
+<body>
+    
+    <header>
+        <nav>
+            <ul>
+                <h1>Ninjas Network</h1>
+                <li><a href="/">Home</a></li>
+                <li><a href="/ninjas">Ninjas</a></li>
+                <li><a href="/ninjas/create">Create New Ninja</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main class="container">
+        {{ $slot }} 
+        <!-- This is where the content of the child views will be injected -->
+    </main>
+</body>
+</html>
